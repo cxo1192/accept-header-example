1ab9598f9d3826b54e22b388e89d1f4ea9790d8f
Cameron Ostrom (RIT Student) <cxo1192@ad.rit.edu>
2019-09-09 14:50:49 -0400

    did accept headers demo

--- src/responses.js
+++ src/responses.js
@@ -12,6 +12,26 @@ const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
 
+const getCats = (request,response, acceptedTypes) =>{
+  const cat = {
+    name: 'Garfield',
+    age: 6,
+  };
+
+
+  if(acceptedTypes[0] === 'text/xml'){
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <name>${cat.name}</name>`;
+    responseXML = `${responseXML} <age>${cat.age}</age>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request,response,responseXML,'text/xml');
+  }
+
+  const catString = JSON.stringify(cat);
+  return respond(request,response,catString,'application/json');
+};
+
 module.exports = {
   getCats,
   getIndex,
